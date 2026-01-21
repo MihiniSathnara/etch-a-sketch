f41397729a84b8d1b72bfabc807daf84762ea334
MihiniSathnara <smihinis@gmail.com>
2026-01-21 18:18:32 +0530

    Add generate grid functionality according to user input Use a prompt to get the grid size from the user and generate a new grid of the desired number of boxes

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
-let box;
+
 const container = document.getElementById("container");
-const squareSize = (container.clientWidth / 20);
+const initialSquareSize = (container.clientWidth / 20);
 
 for(let i=0; i<20; i++){
     for(let j=0; j<20; j++){
-        box = document.createElement("div");
-        box.setAttribute("class", "box");
-        box.style.width = `${squareSize}px`;
-        box.style.height = `${squareSize}px`;
-        container.appendChild(box);
+        const initialBox = document.createElement("div");
+        initialBox.setAttribute("class", "box");
+        initialBox.style.width = `${initialSquareSize}px`;
+        initialBox.style.height = `${initialSquareSize}px`;
+
+        initialBox.addEventListener("mouseenter", () => {
+            initialBox.style.backgroundColor = "black";
+        });
+
+        container.appendChild(initialBox);
+    }
+}
+
+let resizeGridButton = document.addEventListener("click", () => {
+    let boxesPerRow = parseInt(prompt("Enter number of boxes per row (1-100):"));
+    
+    if(boxesPerRow>100 || boxesPerRow<1 || boxesPerRow===null){
+        alert("please enter a number between 1-100");
+        return;
+    }
+
+    
+    const squareSize = (container.clientWidth / boxesPerRow);
+
+    generateGrid(boxesPerRow, squareSize);  
+});
+
+function generateGrid(boxesPerRow, squareSize){
+    container.innerHTML = "";
+    console.log(boxesPerRow);
+    console.log(squareSize);
+
+    for(let i=0; i<boxesPerRow; i++){
+        
+        for(let j=0; j<boxesPerRow; j++){
+            const box = document.createElement("div");
+            box.setAttribute("class", "box");
+            box.style.width = `${squareSize}px`;
+            box.style.height = `${squareSize}px`;
+
+            box.addEventListener("mouseenter", () => {
+                box.style.backgroundColor = "black";
+            });
+
+            container.appendChild(box);
+        }
     }
 }
